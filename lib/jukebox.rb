a4cc59c5e638bc92dfc84cd4b9eda63947423fa7
github username <gregm9999@gmail.com>
2019-09-08 12:57:51 -0700

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,54 @@
-# Add your code here
+require "pry"
+
+def run(songs)
+	while true do
+		puts "Please enter a command:"
+		user_input = gets.chomp
+		case user_input
+			when "play"
+				play(songs)
+			when "help"
+				help
+			when "list"
+				list(songs)
+			when "exit"
+				exit_jukebox	
+				break
+			else
+				puts "Invalid entry"
+		end
+	end
+end
+
+
+def help
+  puts "I accept the following commands:"
+	puts "- help : displays this help message"
+	puts "- list : displays a list of songs you can play"
+	puts "- play : lets you choose a song to play"
+	puts "- exit : exits this program"
+end
+
+def list(songs)
+	songs.each_with_index do |song, index|
+		puts "#{index+1}. #{song}"
+	end
+end
+
+
+def play(songs)
+	puts "Please enter a song name or number:"
+	user_input = gets.strip
+	if user_input.to_i >=1 && user_input.to_i <= songs.length
+		puts "Playing #{songs[user_input.to_i-1]}"
+	elsif songs.include?(user_input)
+		puts "Playing #{songs.find{|song| song == user_input}}"
+	else
+		puts "Invalid input, please try again"
+	end
+end
+
+def exit_jukebox
+	puts "Goodbye"
+end
+
